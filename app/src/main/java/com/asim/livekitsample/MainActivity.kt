300e9c3e9a45ae27809807bd85c096e232d5d1a5
Asim <asimpsy11@gmail.com>
2024-01-15 16:16:13 +0100

    LiveKit implementation done with sample token

--- app/src/main/java/com/asim/livekitsample/MainActivity.kt
+++ app/src/main/java/com/asim/livekitsample/MainActivity.kt
@@ -2,10 +2,121 @@ package com.asim.livekitsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import io.livekit.android.LiveKit
+import io.livekit.android.events.RoomEvent
+import io.livekit.android.events.collect
+import io.livekit.android.renderer.SurfaceViewRenderer
+import io.livekit.android.room.Room
+import io.livekit.android.room.track.Track
+import io.livekit.android.room.track.VideoTrack
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var room: Room
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        // Create Room object.
+        room = LiveKit.create(applicationContext)
+
+        // Setup the video renderer
+        room.initVideoRenderer(findViewById<SurfaceViewRenderer>(R.id.renderer))
+
+        requestNeededPermissions { connectToRoom() }
+    }
+
+    private fun connectToRoom() {
+        val url = "wss://your_host"
+        val token = "your_token"
+
+        lifecycleScope.launch {
+            // Setup event handling.
+            launch {
+                room.events.collect { event ->
+                    when (event) {
+                        is RoomEvent.TrackSubscribed -> onTrackSubscribed(event)
+                        else -> {}
+                    }
+                }
+            }
+
+            // Connect to server.
+            room.connect(
+                url,
+                token,
+            )
+
+            // Turn on audio/video recording.
+            val localParticipant = room.localParticipant
+            localParticipant.setMicrophoneEnabled(true)
+            localParticipant.setCameraEnabled(true)
+
+            // Attach video of remote participant if already available.
+            val remoteVideoTrack = room.remoteParticipants.values.firstOrNull()
+                ?.getTrackPublication(Track.Source.CAMERA)
+                ?.track as? VideoTrack
+
+            if (remoteVideoTrack != null) {
+                attachVideo(remoteVideoTrack)
+            }
+        }
+    }
+
+    private fun onTrackSubscribed(event: RoomEvent.TrackSubscribed) {
+        val track = event.track
+        if (track is VideoTrack) {
+            attachVideo(track)
+        }
+    }
+
+    private fun attachVideo(videoTrack: VideoTrack) {
+        videoTrack.addRenderer(findViewById<SurfaceViewRenderer>(R.id.renderer))
+        findViewById<View>(R.id.progress).visibility = View.GONE
+    }
+
+    private fun requestNeededPermissions(onHasPermissions: () -> Unit) {
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { grants ->
+                var hasDenied = false
+                // Check if any permissions weren't granted.
+                for (grant in grants.entries) {
+                    if (!grant.value) {
+                        Toast.makeText(this, "Missing permission: ${grant.key}", Toast.LENGTH_SHORT).show()
+
+                        hasDenied = true
+                    }
+                }
+
+                if (!hasDenied) {
+                    onHasPermissions()
+                }
+            }
+
+        // Assemble the needed permissions to request
+        val neededPermissions = listOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA)
+            .filter { ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_DENIED }
+            .toTypedArray()
+
+        if (neededPermissions.isNotEmpty()) {
+            requestPermissionLauncher.launch(neededPermissions)
+        } else {
+            onHasPermissions()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        room.disconnect()
     }
 }
